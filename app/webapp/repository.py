1515710e7ed8ef5b2e08dd8deb7099eb3d36343f
Mohammad Sabzalian <mohammad.sabzalian83@gmail.com>
2026-07-26 17:42:35 +0330

    fix: fenart fetch4

--- app/webapp/repository.py
+++ app/webapp/repository.py
@@ -1144,7 +1144,6 @@ async def _lazy_fetch_tracks() -> None:
         FROM tracks
         WHERE performer IS NOT NULL AND title IS NOT NULL
           AND cover_id IS NULL
-          AND COALESCE((metadata->>'lastfm_synced')::boolean, false) = false
           AND id > $1
         ORDER BY id ASC
         LIMIT $2;
@@ -1202,7 +1201,6 @@ async def _lazy_fetch_artists() -> None:
         FROM artists
         WHERE name IS NOT NULL
           AND (cover_id IS NULL OR description IS NULL)
-          AND COALESCE((metadata->>'lastfm_synced')::boolean, false) = false
           AND id > $1
         ORDER BY id ASC
         LIMIT $2;
